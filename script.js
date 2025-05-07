7167dc1045143d24c1144ff09d524bb60ffa8452
Alex Deliberali <alexdeliberali@gmail.com>
2025-05-07 19:28:44 -0300

    Start study JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const animais = document.getElementById("animais");
+
+//Seleciona todos os componentes que começa com #, funciona como o LIKE nas consultas de banco de dados
+const linkInterno = document.querySelector("[href^='#']")
